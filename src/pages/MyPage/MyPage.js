4c5376bd22350722df688f9f78132f932002b031
You Hyun sang <pycandyq@naver.com>
2023-08-16 13:57:22 +0900

    {수정} mypage 스타일 변경

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -15,52 +15,41 @@ const MyPage = () => {
     const navigate = useNavigate()
     
 
-  
-
-  useEffect(() => {
-    // useEffect 안에서 Axios로 데이터를 가져옴
-    axios.get('http://localhost:8080/main/login')
-      .then(response => {
-       
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching user details:', error);
-      });
-  }, []); // 컴포넌트가 마운트될 때 한 번만 실행
-
-
-    // useEffect(() => {
-    //     const getData = async () => {
-    //         try {
-    //             const response = await axios.get("http://localhost:8080/main/user/detail");
-    //             console.log(response.data);
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     }
-    //     getData();
-    // }, []);
-       
-
     const backReturnPage = () => {
         navigate("/");
     }
 
     return (
         <div className="main-components-container ">
-            <MainBodyWrapper>
-                <div className="main-mypage-wrapper">
-                    <img className="background-picture" src='/img/background1.jpg' alt='배경1'/>
-                    <p>이름: 이름</p>
-                    <p>하고싶은말: 말말</p>
-                    <p>이메일: 이메일아이디</p>
-                    <p>오픈쳇: 오픈쳇주소</p>
-                    <button>회원 탈퇴</button>
-                    <button>회원 정보 수정</button>
-                    <button onClick={backReturnPage}>뒤로가기</button>
+            <div className="main-mypage-header">
+                    <h2>내 정보 보기</h2>
+            </div>
+                <div className="main-mypage-wrapper-body">
+                    <div className="main-mypage-wrapper-nickname">
+                        <img className="background-picture" src='/img/background1.jpg' alt='배경1'/>
+                        <p>닉네임</p>
+                        <span>유현상</span>
+                    </div>
+                    <div className="main-mypage-wrapper-content"> 
+                        <p>하고싶은말</p>
+                        <p>저는 유현상입니다!</p>
+                        <p>이메일</p>
+                        <p>pycandyq@naver.com</p>
+                        <p>오픈쳇</p>
+                        <p>openchat@jusojuso</p>
+                        <p>배우고싶은 재능 <span>IT {'>'} PROGRAMMING</span></p>
+                        <p>자바</p>
+                        <p>가르치고싶은 재능 <span>예체능 {'>'} 헬스</span> </p>
+                        <p>식단조절방법</p>
+                        <div className="main-mypage-wrapper-content-button">
+                        <button>회원 탈퇴</button>
+                        <button>회원 정보 수정</button>
+                        <button onClick={backReturnPage}>뒤로가기</button>
+                        </div>
+                    </div>
                 </div>
-            </MainBodyWrapper>
+                
+            
         </div>
     )
 }
